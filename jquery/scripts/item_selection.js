8e0be92e0c793440cd48d7d88b7df24c5d42d51a
Charan Shampur <cshampur@cisco.com>
2017-11-24 12:22:44 -0800

    Adding jquery to the bundle.js

--- jquery/scripts/item_selection.js
+++ jquery/scripts/item_selection.js
@@ -1,86 +1,44 @@
+let $ = require('jquery/dist/jquery.slim');
+
+let Direction = {
+    LeftToRight: 0,
+    RightToLeft: 1
+};
+
 class ItemSelection {
     constructor() {
-        this.list = [];
-        this.bucket = [];
-        this.populateList();
         this.setup();
     }
 
     setup() {
         let that = this;
-        $('.container .list .item').click(function(event) {
-            $(this).toggleClass('selected');
-            if ($(this).hasClass('selected')) {
-                let index = $(this).index();
-                that.list[index].selected = true;
-            }
-        });
+        this.list = $('.container .list');
+        this.bucket = $('.container .bucket');
 
-        $('.container .bucket .item').click(function(event) {
+        $('.container .item').click(function(event) {
             $(this).toggleClass('selected');
-            if ($(this).hasClass('selected')) {
-                let index = $(this).index();
-                that.bucket[index].selected = true;
-            }
         });
 
         $('.container .controls .button.moveRight').click(function(event) {
-            that.moveSelectedRight();
+            that.moveSelected('.container .list .item', Direction.LeftToRight);
         });
 
         $('.container .controls .button.moveLeft').click(function(event) {
-            that.moveSelectedLeft();
+            that.moveSelected('.container .bucket .item', Direction.RightToLeft);
         });
     }
 
-    populateList() {
+    moveSelected(selection, direction) {
         let that = this;
-        $('.container .list .item').each(function(index) {
-            let elem = this;
-            that.list.push({item: elem, selected: false});
-        });
-    }
-
-    moveSelectedRight(){
-        let that = this;
-        let newList = [];
-        this.list.forEach((element, index) => {
-            if (element.selected) {
-                $('.container .bucket').append($(element.item));
-                that.bucket.push(element);
-            } else {
-                newList.push(element);
+        $(selection).each(function(index) {
+            if($(this).hasClass('selected')){
+                if (direction === Direction.LeftToRight) {
+                    that.bucket.append($(this).removeClass('selected'));
+                }
+                if (direction === Direction.RightToLeft) {
+                    that.list.append($(this).removeClass('selected'));
+                }
             }
-        });
-        this.list = newList;
-        this.reset();
-    }
-
-    moveSelectedLeft(){
-        let that = this;
-        let newList = [];
-        this.bucket.forEach((element, index) => {
-            if (element.selected) {
-                $('.container .list').append($(element.item));
-                that.list.push(element);
-            } else {
-                newList.push(element);
-            }
-        });
-        this.bucket = newList;
-        this.reset();
-    }
-
-    reset() {
-        let that = this;
-        $('.container .list .item').each(function(index) {
-            that.list[index]['selected'] = false;
-            $(this).removeClass('selected');
-        });
-
-        $('.container .bucket .item').each(function(index) {
-            that.bucket[index]['selected'] = false;
-            $(this).removeClass('selected');
         });
     }
 }
--- jquery/scripts/main.js
+++ jquery/scripts/main.js
@@ -1,3 +1,4 @@
+let $ = require('jquery/dist/jquery.slim');
 let ItemSelection = require('./item_selection');
 $(document).ready(() => {
     let item = new ItemSelection();
