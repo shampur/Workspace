5a7afcfef9c2266c76db1660c87c81179760f89a
Charan Shampur <charanshampur@gmail.com>
2017-12-26 11:12:18 -0800

    Some more arrow function examples
    Signed-off-by: Charan Shampur <charanshampur@gmail.com>

--- javascript_concepts/differences_arrow_functions.js
+++ javascript_concepts/differences_arrow_functions.js
@@ -7,25 +7,67 @@
     c) Arrow functions provide a static context whereas function expressions provide a dynamic context.
  */
 
-var name = 'Ashwin';
-var myObj = {
-    name: 'bharath',
-    printNameExpr: function () {
-        console.log('Name = ' + this.name);
-        /*
-        return function() { // Here this gets its value dynamically based on the caller.
+(() => {
+    var name = 'Ashwin';
+    var myObj = {
+        name: 'bharath',
+        printNameExpr: function () {
             console.log('Name = ' + this.name);
+            /*
+            return function() { // Here this gets its value dynamically based on the caller.
+                console.log('Name = ' + this.name);
+            }
+            */
+            return () => {
+                console.log('Name = ' + this.name); // Here this gets its value from the surrounding scope which is the same
+                                                    // scope as that of the printNameExpr function.
+            }
+        },
+        printNameArrow: () => {
+            console.log('Name = ' + this.name); // this gets its value from the surrounding scope window object.
         }
-        */
-        return () => {
-            console.log('Name = ' + this.name); // Here this gets its value from the surrounding scope which is the same
-                                                // scope as that of the printNameExpr function.
-        }
-    },
-    printNameArrow: () => {
-        console.log('Name = ' + this.name); // this gets its value from the surrounding scope window object.
-    }
-};
+    };
+    myObj.printNameExpr()();
+    myObj.printNameArrow();
+})();
 
-myObj.printNameExpr()();
-myObj.printNameArrow();
+(function() {
+    var myobj = {
+        name: 'bharath',
+        printNameAfterExpr() {
+            setTimeout(function() {
+                console.log('Name = ' + this.name); // Since timeout function runs asynchronously the value of this when
+                                                    // the function is called would be window
+            }, 1000)
+        },
+        printNameAfterArrow() {
+            setTimeout(() => {
+                console.log('Name = ' + this.name); // The value of this is statically assigned by the arrow function depending
+                                                    // upon the surrounding scope of the function which in this case happens
+                                                    // to be myobj;
+            }, 1000)
+        }
+    };
+    myobj.printNameAfterExpr();
+    myobj.printNameAfterArrow();
+})();
+
+
+/*
+    More examples of usage of arrow functions. It replaces the use of assigning that = this
+ */
+(() => {
+   var obj = {
+       name: 'bharath',
+       printName() {
+           return this.async(() => {
+               console.log('This name = ' + this.name);
+           })
+       },
+       async(f){
+           f();
+           f.call(obj);
+       }
+   }
+   obj.printName();
+})();
